histogram equalization + fixes in the histogram calculation functions

TP1.py
@@ -69,24 +69,28 @@ def get_image_stats(data, li, lj):
     return mean, math.sqrt(variance)
 
 
-def get_histogram(data, grayscale):
+def get_histogram(data, grayscale, plot_option=False):
     histogram = np.zeros(grayscale+1, dtype=int)
     for row in data:
         for col in row:
             histogram[col] += 1
-    plt.bar(range(grayscale+1), histogram)
-    plt.show()
+    if plot_option:
+        plt.bar(range(grayscale+1), histogram)
+        plt.show()
+    return histogram
 
 
-def get_cumulative_histogram(data, grayscale):
+def get_cumulative_histogram(data, grayscale, plot_option=False):
     response = np.zeros(grayscale+1, dtype=int)
     for row in data:
         for col in row:
             response[col] += 1
     for index in range(1, grayscale+1):
         response[index] += response[index-1]
-    plt.bar(range(grayscale + 1), response)
-    plt.show()
+    if plot_option:
+        plt.bar(range(grayscale + 1), response)
+        plt.show()
+    return response
 
 
 if __name__ == '__main__':
@@ -97,6 +101,6 @@ if __name__ == '__main__':
     # get the mean and standard div
     get_image_stats(data, li, lj)
     # get the histogram
-    get_histogram(data, gray_scale)
+    get_histogram(data, gray_scale, plot_option=True)
     # get the cumulative histogram
-    get_cumulative_histogram(data, gray_scale)
+    get_cumulative_histogram(data, gray_scale, plot_option=True)

TP2.py
@@ -1,5 +1,27 @@
-from TP1 import read_pgm, get
+import math
+
+from TP1 import read_pgm, write_pgm, get_histogram, get_cumulative_histogram
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def equalize_histogram(matrix, height, width, maximum_grayscale, plot_option=False, save_option=True):
+    hc = get_cumulative_histogram(matrix, maximum_grayscale)
+    cumulative_probability = [float(c/(width*height)) for c in hc]
+    grayscale_mapping = [int(math.ceil(c * maximum_grayscale)) for c in cumulative_probability]
+    new_data = np.zeros(width*height, dtype=int)
+    for i in range(height):
+        for j in range(width):
+            new_data[i*width+j] = grayscale_mapping[data[i][j]]
+    new_data = new_data.reshape(height, width)
+    if plot_option:
+        new_histogram = get_histogram(new_data, maximum_grayscale)
+        plt.bar(range(maximum_grayscale+1), new_histogram)
+        plt.show()
+    if save_option:
+        write_pgm('equalized-image', new_data, maximum_grayscale, height, width)
 
 
 if __name__ == "__main__":
-    pass
+    li, lj, gray_scale, data = read_pgm('assets/balloons-P2.pgm')
+    equalize_histogram(data, li, lj, gray_scale, plot_option=True)