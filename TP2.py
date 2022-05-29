34974acf67476f7bd30bb1fd0067c57ee001abd3
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-05-29 21:55:05 +0100

    linear transformation of images's histogram

--- TP2.py
+++ TP2.py
@@ -20,8 +20,31 @@ def equalize_histogram(matrix, height, width, maximum_grayscale, plot_option=Fal
         plt.show()
     if save_option:
         write_pgm('equalized-image', new_data, maximum_grayscale, height, width)
+    return new_data
+
+
+def histogram_linear_transformation(matrix, level0, level1, maximum_grayscale, plot_option=False, save_option=True):
+    height, width = matrix.shape
+    response = np.zeros(height*width, dtype=int)
+    for i, row in enumerate(matrix):
+        for j, col in enumerate(row):
+            if col <= level0:
+                response[width*i+j] += 0
+            elif col >= level1:
+                response[width*i+j] += maximum_grayscale
+            else:
+                response[width*i+j] += maximum_grayscale * (col-level0) /(level1 - level0)
+    response = response.reshape(height, width)
+    if plot_option:
+        histogram = get_histogram(response, grayscale=maximum_grayscale)
+        plt.bar(range(maximum_grayscale+1), histogram)
+        plt.show()
+    if save_option:
+        write_pgm('linear-transformation', response, maximum_grayscale, height, width)
+    return response
 
 
 if __name__ == "__main__":
     li, lj, gray_scale, data = read_pgm('assets/balloons-P2.pgm')
     equalize_histogram(data, li, lj, gray_scale, plot_option=True)
+    histogram_linear_transformation(data, 50, 150, gray_scale, plot_option=True)
