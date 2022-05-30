c4a8750b9474deb3c6b9d0b603dbce88b7bc3622
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-05-30 02:36:15 +0100

    generic convolutional filter and median filter

--- TP3.py
+++ TP3.py
@@ -4,7 +4,7 @@ from TP1 import read_pgm, write_pgm
 from PIL import Image
 
 
-def distort_image(matrix, height, width, max_grayscale):
+def distort_image(matrix, height, width, max_grayscale, save_option= True):
     np.random.seed(0)
     response = np.zeros(width * height, dtype=int)
     for i, row in enumerate(matrix):
@@ -16,26 +16,30 @@ def distort_image(matrix, height, width, max_grayscale):
                 response[width * i+j] = max_grayscale
             else:
                 response[width * i+j] = col
-    return response.reshape(height, width)
+    response = response.reshape(height, width)
+    if save_option:
+        write_pgm('distorted-image', response, max_grayscale, height, width)
+    return response
 
 
-def filter_image(matrix, height, width, applied_filter):
+def filter_image(matrix, height, width, max_grayscale, applied_filter, filter_size, save_option=True):
     response = np.zeros(width*height, dtype=int)
+    skipped = int((filter_size-1)/2)
     for i, row in enumerate(matrix):
-        if i == 0 or i == height-1:
+        if i in range(skipped) or i in range(height - skipped, height):
             continue
         else:
             for j, col in enumerate(row):
-                if j == 0 or j == width -1:
-                    pass
+                if j in range(skipped) or j in range(width - skipped, width):
+                    continue
                 else:
-                    element = np.array([
-                        matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i - 1][j + 1],
-                        matrix[i][j - 1], col, matrix[i][j + 1],
-                        matrix[i + 1][j - 1], matrix[i + 1][j], matrix[i + 1][j + 1]
-                    ]).reshape(3, 3)
-                    response[i*width+j] = convolution_three_by_three(element, applied_filter) if convolution_three_by_three(element, applied_filter) > 0 else 0
-    return response.reshape(height, width)
+                    element = matrix[np.ix_(list(range(i-skipped, i+skipped+1)), list(range(j-skipped, j+skipped+1)))]
+                    response[i*width+j] = convolution_three_by_three(element, applied_filter) \
+                        if convolution_three_by_three(element, applied_filter) > 0 else 0
+    response = response.reshape(height, width)
+    if save_option:
+        write_pgm('convolution-image', response, max_grayscale, height, width)
+    return response
 
 
 def convolution_three_by_three(a, b):
@@ -46,6 +50,26 @@ def convolution_three_by_three(a, b):
     return convolution
 
 
+def median_filter_image(matrix, height, width, max_grayscale, filter_size, save_option=True):
+    response = np.zeros(width * height, dtype=int)
+    skipped = int((filter_size - 1) / 2)
+    for i, row in enumerate(matrix):
+        if i in range(skipped) or i in range(height - skipped, height):
+            continue
+        else:
+            for j, col in enumerate(row):
+                if j in range(skipped) or j in range(width - skipped, width):
+                    continue
+                else:
+                    element = matrix[np.ix_(list(range(i - skipped, i + skipped + 1)), list(range(j - skipped, j + skipped + 1)))]
+                    element = element.reshape(filter_size**2, 1)
+                    response[i * width + j] = np.sort(element)[int((filter_size**2+1)/2)]
+    response = response.reshape(height, width)
+    if save_option:
+        write_pgm('median-image', response, max_grayscale, height, width)
+    return response
+
+
 if __name__ == '__main__':
     li, lj, gray_scale, data = read_pgm('./assets/equalized-image.pgm')
     distorted_image = distort_image(data, li, lj, gray_scale)
@@ -54,15 +78,21 @@ if __name__ == '__main__':
     # plt.show()
 
     # definition of the low pass filter mean of size 3X3
-    low_pass_filter = (1/3) * np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]).reshape(3,3)
+    # low_pass_filter = (1/9) * np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]).reshape(3,3)
 
     # definition of high pass filter of size 3X3
-    high_pass_filter = np.array([0, -1, 0, -1, 5, -1, 0, -1, 0]).reshape(3, 3)
+    # high_pass_filter = np.array([0, -1, 0, -1, 5, -1, 0, -1, 0]).reshape(3, 3)
 
-    filtered_low = filter_image(distorted_image, li, lj, low_pass_filter)
-    plt.imshow(Image.fromarray(filtered_low))
+    # filtered_low = filter_image(distorted_image, li, lj, gray_scale, low_pass_filter, 3)
+    # plot low filtered image
+    # plt.imshow(Image.fromarray(filtered_low))
+    # plt.show()
+
+    # filtered_high = filter_image(distorted_image, li, lj, gray_scale, high_pass_filter, 3)
+    # plot high filtered image
+    # plt.imshow(Image.fromarray(filtered_high))
+    # plt.show()
+    median_filtered_image = median_filter_image(distorted_image, li, lj, gray_scale, 3)
+    plt.imshow(Image.fromarray(median_filtered_image))
     plt.show()
 
-    filtered_high = filter_image(distorted_image, li, lj, high_pass_filter)
-    plt.imshow(Image.fromarray(filtered_high))
-    plt.show()
